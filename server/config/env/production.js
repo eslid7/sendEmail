3e8231baeffa1e5407d17dcc7e0025aa6ec07c75
Esli Gonzalez <eslid1@gmail.com>
2020-06-02 11:45:35 -0600

    se agrega el de produccion

---
+++ server/config/env/production.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  	from: 'TramiteYa <notificaciones@tramiteya.go.cr>',
+	to: 'notificaciones@tramiteya.go.cr',
+	host: 'smtp.racsa.co.cr',
+	port: 5050,
+	portEmail : 25,
+	secure: false,
+	pool: true,
+	maxConnections: 1,
+  	user: 'notificaciones@tramiteya.go.cr',
+	password: 'Zxcv4321++',
+	rejectUnauthorized: false,
+	appName : 'TramiteYa',
+	token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJyeCIsImluc19wayI6MjYxLCJhcHBfbmFtZSI6IiIsImV4cCI6MTYwMzUxMDYyNDQyMiwiaWF0IjoxNTcxOTc0NjI0NDIyfQ.5PdgY_rknZgqxWB9M0aJOPpH3A9h8VnjKxQrGyxmqjhsj2pJW3_obqKTnJlgzjzt5rPUHDgam0CdK_xStNr-Gg'
+}
